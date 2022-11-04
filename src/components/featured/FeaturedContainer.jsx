4a1b45e51638597983fc0b4ea87d5e36f8da255b
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-11-04 19:54:39 +0530

    nikhilesh

--- src/components/featured/FeaturedContainer.jsx
+++ src/components/featured/FeaturedContainer.jsx
@@ -1,11 +1,20 @@
 
 
 import React from 'react'
+import FeaturedPost from './FeaturedPost'
 
 function FeaturedContainer() {
     return (
         <>
-            Featured GRID
+            <div className="w-full h-full bg-slate-500 flex flex-row gap-2 justify-center p-4">
+                <FeaturedPost />
+                <div className='h-[22.5rem] bg-blue-200 w-[641px]'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/featured/FeaturedPost.jsx
+++ src/components/featured/FeaturedPost.jsx
@@ -1,11 +1,18 @@
 
 
 import React from 'react'
-
+import featured from '../../assets/images/featured.jpg';
 function FeaturedPost() {
     return (
-        <div className="h-10 w-10 bg-red-400">
-
+        <div className="h-[508px] bg-white w-[641px]">
+            <div className="h-[361px] w-[641px] overflow-hidden">
+                <img width={641} height={361} src={featured} alt="" />
+                <div className="capitalize text-[12px] color-[#ffd200]">TV SERIES</div>
+            </div>
+            <div className="pt-[7px] pb-[12px] px-[14px]">
+                <h3 className="text-[25.5px] font-semibold">Kung Fu Panda: The Dragon Knight Season 2 Potential Release Date and More</h3>
+                <p>Kung Fu Panda: THe Dragon Knight Season 2 has already not been announced and here we have its Expected Release Data, Cast and possible Plot info</p>
+            </div>
         </div>
     )
 }
