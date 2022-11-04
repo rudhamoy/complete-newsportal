ae4377b264fdc21201540d5cf9a76919698f440d
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-11-04 20:56:33 +0530

    nikhilesh

--- src/components/featured/FeaturedContainer.jsx
+++ src/components/featured/FeaturedContainer.jsx
@@ -1,18 +1,20 @@
 
 
 import React from 'react'
+
 import FeaturedPost from './FeaturedPost'
+import MasonaryItem from './MasonaryItems'
 
 function FeaturedContainer() {
     return (
         <>
-            <div className="w-full h-full bg-slate-500 flex flex-row gap-2 justify-center p-4">
+            <div className="w-full h-full bg-slate-200 flex flex-row gap-2 justify-center p-4">
                 <FeaturedPost />
-                <div className='h-[22.5rem] bg-blue-200 w-[641px]'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className="grid grid-cols-2 gap-2">
+                    <MasonaryItem />
+                    <MasonaryItem />
+                    <MasonaryItem />
+                    <MasonaryItem />
                 </div>
             </div>
         </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import FeaturedContainer from '../components/featured/FeaturedContainer'
-import FeaturedPost from '../components/featured/FeaturedPost'
+
 
 const Home = () => {
   return (
     <>
-        <FeaturedPost />
+      < FeaturedContainer />
     </>
   )
 }
