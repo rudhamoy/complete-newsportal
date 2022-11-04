9e6914778e6a716b6fee47f467a1f47651073148
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-11-04 21:41:31 +0530

    nikhilesh

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./fonts/FUTURA21.ttf"
 import Home from './pages/Home';
 import Author from './pages/Author';
+import Archeive from './pages/Archeive';
 import Layout from './components/layout/Layout';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/author" element={<Author />} />
+          <Route path="/archeive" element={<Archeive />} />
         </Routes>
       </BrowserRouter>
     </Layout>
