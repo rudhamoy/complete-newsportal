6ede5df6d7496e326c887b7c0bc9c343aebe1470
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-11-04 19:52:12 +0530

    updated with router and added font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import FeaturedContainer from './components/featured/FeaturedContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./fonts/FUTURA21.ttf"
+import Home from './pages/Home';
+import Author from './pages/Author';
 
 function App() {
   return (
-    <>
-      <FeaturedContainer />
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/author" element={<Author />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FeaturedContainer from '../components/featured/FeaturedContainer'
+import FeaturedPost from '../components/featured/FeaturedPost'
+
+const Home = () => {
+  return (
+    <>
+        <FeaturedPost />
+    </>
+  )
+}
+
+export default Home
