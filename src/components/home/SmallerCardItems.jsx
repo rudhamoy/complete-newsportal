902b5c0be8055aae5f0d20ec7ea69d70ea6e4a21
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-11-15 22:53:30 +0530

    icon_added_nikhilesh

--- src/components/home/SmallerCardItems.jsx
+++ src/components/home/SmallerCardItems.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import couplesHug from '../../assets/images/couples_hug.png';
+import videoIcon from '../../assets/icons/video-icon.svg';
 function SmallerCardItems() {
     return (
         <div className="p-2 py-[10px] rounded-[2px] border border-[#e4e4e4] bg-white my-1 cursor-pointer h-[130px] w-[398px]" >
             <div className="flex gap-x-2">
-                <div className="h-[108px]  w-[208px]">
+                <div className="h-[108px]  w-[208px] relative">
                     <img className="w-[100%] h-[100%] rounded-md" src={couplesHug} alt="" />
+                    <div className="absolute  z-[10] bottom-2 sm:left-2  left-2 flex flex-row justify-start gap-1 items-center drop-shadow-3xl ">
+                        <img src={videoIcon} width={15} height={15} className="text-[#ffd200] font-bold" alt="" />
+                        {/* <div className="capitalize text-[8px] text-[#ffd200] font-nunitoSans  font-bold">NOVEMBER 2, 2022</div> */}
+                    </div>
                 </div>
                 <div className="relative flex flex-col justify-between">
                     <p className="text-[10px] text-[#bf912d] font-bold">TV SERIES</p>
--- src/components/sidebar/SideBarItem.jsx
+++ src/components/sidebar/SideBarItem.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-
+import videoIcon from '../../assets/icons/video-icon.svg';
 function SideBarItem({ category, title, image }) {
     return (
 
         <div className="flex flex-row items-center p-2 cursor-pointer rounded-[2px] w-[90vw] sm:w-[383px] h-[108px] z-0 border">
-                <div className="h-[85px] w-[85px] overflow-hidden rounded-sm">
-                    <img className="h-[100%] w-[100%]" src={image} alt="" />
+            <div className="h-[85px] w-[85px] overflow-hidden rounded-sm relative">
+                <img className="h-[100%] w-[100%]" src={image} alt="" />
+                <div className="absolute  z-[10] bottom-2 sm:left-2  left-2 flex flex-row justify-start gap-1 items-center drop-shadow-3xl ">
+                    <img src={videoIcon} width={15} height={15} className="text-[#ffd200] font-bold" alt="" />
+                    {/* <div className="capitalize text-[8px] text-[#ffd200] font-nunitoSans  font-bold">NOVEMBER 2, 2022</div> */}
                 </div>
-                <div className="px-2 relative h-[80px] w-[250px]">
-                    <p className="font-futura2 text-[#bf912d] font-bold text-[10px] capitalize">{category}</p>
-                    <p className="font-nunitoSans text-[16px] leading-5 text-black font-semibold">
-                        {title}
-                    </p>
-                </div>
+            </div>
+            <div className="px-2 relative h-[80px] w-[250px]">
+                <p className="font-futura2 text-[#bf912d] font-bold text-[10px] capitalize">{category}</p>
+                <p className="font-nunitoSans text-[16px] leading-5 text-black font-semibold">
+                    {title}
+                </p>
+            </div>
         </div>
 
     )
